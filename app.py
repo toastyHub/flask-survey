30f73979a267b66727f2c622d467dc46c6070eef
toasty <daniel.m84@outlook.com>
2023-03-08 12:12:58 -0500

    Add session functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -9,16 +9,28 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+# responses = []
 
-responses = []
+# *****************************************************************************
 
 @app.route('/')
 def survey_home():
     # also pass satisfaction_survey as survey (survey=survey) with the rendered template to use in html 
     return render_template('survey_home.html', survey=survey)
 
+# *****************************************************************************
+
+@app.route('/begin', methods=['POST'])
+def set_session():
+    session["responses"] = []
+    return redirect('/questions/0')
+
+# *****************************************************************************
+
 @app.route('/questions/<int:qid>')
 def show_question(qid):
+    
+    responses = session.get("responses")
     
     if (qid != len(responses)):
         flash(f"You're trying to access an invalid question id: {qid}.")
@@ -28,12 +40,19 @@ def show_question(qid):
         return redirect("/")
     
     question = survey.questions[qid]
+    
     return render_template("/question.html", question_num=qid, question=question)
+
+# *****************************************************************************
 
 @app.route('/answer', methods=["POST"])
 def handle_answer():
+    
+    responses = session["responses"]
+    
     choice = request.form['answer']
     responses.append(choice)
+    session["responses"] = responses
     
     if (len(responses) == len(survey.questions)):
         return render_template("/thankyou.html")
